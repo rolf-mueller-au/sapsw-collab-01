8093045e63e5fc9bcf55fee7753f7a81692b7269
Rolf Mueller <rolf.mueller.au@gmail.com>
2012-03-11 01:58:07 +1100

    added loadPernrDetails()

--- jive/collab-pf-pch-01/main.js
+++ jive/collab-pf-pch-01/main.js
@@ -80,5 +80,33 @@ function onClickSave() {
 		});
 }
 
+//--- for loadPernrDetails, we call the backend web-service...
+function loadPernrDetails() {
+    var lf_url = "http://213.23.110.71:8000/sap/bc/srt/rfc/sap/zmur_hcm_collab/801/zmur_hcm_collab/zmur_hcm_collab";
+    var lf_soapEnvelope_1 = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:sap-com:document:sap:rfc:functions\"><soapenv:Header/><soapenv:Body><urn:ZMUR_HCM_GET_EE_NAME><IM_F_PERNR>";
+    var lf_soapEnvelope_2 = "</IM_F_PERNR></urn:ZMUR_HCM_GET_EE_NAME></soapenv:Body></soapenv:Envelope>";
+    var lf_soapEnvelope = lf_soapEnvelope_1 + pa_pernr.value + lf_soapEnvelope_2;
+
+    var lf_params = {};
+    lf_params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.DOM;
+    lf_params[gadgets.io.RequestParameters.AUTHORIZATION] = gadgets.io.AuthorizationType.NONE;
+    lf_params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.POST;
+    lf_params[gadgets.io.RequestParameters.HEADERS] = {
+        "SOAPAction": "ZmurCollabGetWorklistRequest",
+        "Content-Type": "text/xml; charset=utf-8"
+    };
+    lf_params[gadgets.io.RequestParameters.POST_DATA] = lf_soapEnvelope;
+    gadgets.io.makeRequest(lf_url, responseLoadPernrDetails, lf_params);
+}
+
+//--- ...to receive the pernrDetails
+function responseDom(obj) {
+    var lf_domdata = obj.data;
+    var lf_el_display_name = lf_domdata.getElementsByTagName('EX_F_DISPLAY_NAME')[0];
+    var lf_display_name = lf_el_display_name.childNodes[0].nodeValue;
+    pa_name.value = lf_display_name;
+}
+
+
 //--- Register our on-view-load handler
 gadgets.util.registerOnLoadHandler(init);
