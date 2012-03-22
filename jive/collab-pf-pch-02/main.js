8e917108f705a144e53796a139c2687aa0e4d913
Rolf Mueller <rolf.mueller.au@gmail.com>
2012-03-23 01:32:34 +1100

    V2.1

--- jive/collab-pf-pch-02/main.js
+++ jive/collab-pf-pch-02/main.js
@@ -145,35 +145,47 @@ function responseLoadPernrDetails(obj) {
 function checkAppData() {
     mini.createDismissibleMessage("checkAppData() started...");
     var lf_url = "http://213.23.110.71:8000/sap/bc/srt/rfc/sap/zmur_hcm_collab/801/zmur_hcm_collab/zmur_hcm_collab";
-    var lf_soapEnvelope_1 = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:sap-com:document:sap:rfc:functions\"><soapenv:Header/><soapenv:Body><urn:ZMUR_HCM_PNF_PCH_CHECK>";
-    var lf_soapEnvelope_2 = "</urn:ZMUR_HCM_PNF_PCH_CHECK></soapenv:Body></soapenv:Envelope>";
+    var lf_soapEnvelope_beg = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:sap-com:document:sap:rfc:functions\"><soapenv:Header/><soapenv:Body><urn:ZMUR_HCM_PNF_PCH_CHECK>";
+    var lf_soapEnvelope_end = "</urn:ZMUR_HCM_PNF_PCH_CHECK></soapenv:Body></soapenv:Envelope>";
     var lf_soapEnvelope_Pernr = "<IM_F_PERNR>" + pa_pernr.value + "</IM_F_PERNR>";
-    var lf_soapEnvelope_Btrtl = "<IM_F_BTRTL_NEW>" + pa_btrtl_new.value + "</IM_F_BTRTL_NEW>";
-    var lf_soapEnvelope_Bukrs = "<IM_F_BUKRS_NEW>" + pa_bukrs_new.value + "</IM_F_BUKRS_NEW>";
-    var lf_soapEnvelope_Kostl = "<IM_F_KOSTL_NEW>" + pa_kostl_new.value + "</IM_F_KOSTL_NEW>";
-    var lf_soapEnvelope_Orgeh = "<IM_F_ORGEH_NEW>" + pa_orgeh_new.value + "</IM_F_ORGEH_NEW>";
-    var lf_soapEnvelope_Plans = "<IM_F_PLANS_NEW>" + pa_plans_new.value + "</IM_F_PLANS_NEW>";
-    var lf_soapEnvelope_Sachp = "<IM_F_SACHP_NEW>" + pa_sachp_new.value + "</IM_F_SACHP_NEW>";
-    var lf_soapEnvelope_Stell = "<IM_F_STELL_NEW>" + pa_stell_new.value + "</IM_F_STELL_NEW>";
-    var lf_soapEnvelope_Werks = "<IM_F_WERKS_NEW>" + pa_werks_new.value + "</IM_F_WERKS_NEW>";
+//    var lf_soapEnvelope_Btrtl = "<IM_F_BTRTL_NEW>" + pa_btrtl_new.value + "</IM_F_BTRTL_NEW>";
+//    var lf_soapEnvelope_Bukrs = "<IM_F_BUKRS_NEW>" + pa_bukrs_new.value + "</IM_F_BUKRS_NEW>";
+//    var lf_soapEnvelope_Kostl = "<IM_F_KOSTL_NEW>" + pa_kostl_new.value + "</IM_F_KOSTL_NEW>";
+//    var lf_soapEnvelope_Orgeh = "<IM_F_ORGEH_NEW>" + pa_orgeh_new.value + "</IM_F_ORGEH_NEW>";
+//    var lf_soapEnvelope_Plans = "<IM_F_PLANS_NEW>" + pa_plans_new.value + "</IM_F_PLANS_NEW>";
+//    var lf_soapEnvelope_Sachp = "<IM_F_SACHP_NEW>" + pa_sachp_new.value + "</IM_F_SACHP_NEW>";
+//    var lf_soapEnvelope_Stell = "<IM_F_STELL_NEW>" + pa_stell_new.value + "</IM_F_STELL_NEW>";
+//    var lf_soapEnvelope_Werks = "<IM_F_WERKS_NEW>" + pa_werks_new.value + "</IM_F_WERKS_NEW>";
 
-    var lf_soapEnvelope = lf_soapEnvelope_1 + lf_soapEnvelope_Btrtl
-                                            + lf_soapEnvelope_Bukrs
-                                            + lf_soapEnvelope_Kostl
-                                            + lf_soapEnvelope_Orgeh
-                                            + lf_soapEnvelope_Pernr
-                                            + lf_soapEnvelope_Plans
-                                            + lf_soapEnvelope_Sachp
-                                            + lf_soapEnvelope_Stell
-                                            + lf_soapEnvelope_Werks + lf_soapEnvelope_2;
+    var lf_soapEnvelope_Btrtl = "<IM_F_BTRTL_NEW>" + "</IM_F_BTRTL_NEW>";
+    var lf_soapEnvelope_Bukrs = "<IM_F_BUKRS_NEW>" + "</IM_F_BUKRS_NEW>";
+    var lf_soapEnvelope_Kostl = "<IM_F_KOSTL_NEW>" + "</IM_F_KOSTL_NEW>";
+    var lf_soapEnvelope_Orgeh = "<IM_F_ORGEH_NEW>" + "</IM_F_ORGEH_NEW>";
+    var lf_soapEnvelope_Plans = "<IM_F_PLANS_NEW>" + "</IM_F_PLANS_NEW>";
+    var lf_soapEnvelope_Sachp = "<IM_F_SACHP_NEW>" + "</IM_F_SACHP_NEW>";
+    var lf_soapEnvelope_Stell = "<IM_F_STELL_NEW>" + "</IM_F_STELL_NEW>";
+    var lf_soapEnvelope_Werks = "<IM_F_WERKS_NEW>" + "</IM_F_WERKS_NEW>";
+
+    var lf_soapEnvelope;
+    lf_soapEnvelope = lf_soapEnvelope_beg
+        + lf_soapEnvelope_Btrtl
+        + lf_soapEnvelope_Bukrs
+        + lf_soapEnvelope_Kostl
+        + lf_soapEnvelope_Orgeh
+        + lf_soapEnvelope_Pernr
+        + lf_soapEnvelope_Plans
+        + lf_soapEnvelope_Sachp
+        + lf_soapEnvelope_Stell
+        + lf_soapEnvelope_Werks
+        + lf_soapEnvelope_end;
 
     var lf_params = {};
     lf_params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.DOM;
     lf_params[gadgets.io.RequestParameters.AUTHORIZATION] = gadgets.io.AuthorizationType.NONE;
     lf_params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.POST;
     lf_params[gadgets.io.RequestParameters.HEADERS] = {
-        "SOAPAction": "ZmurCollabGetWorklistRequest",
-        "Content-Type": "text/xml; charset=utf-8"
+        "SOAPAction":"ZmurCollabGetWorklistRequest",
+        "Content-Type":"text/xml; charset=utf-8"
     };
     lf_params[gadgets.io.RequestParameters.POST_DATA] = lf_soapEnvelope;
     gadgets.io.makeRequest(lf_url, responseCheckAppData, lf_params);
