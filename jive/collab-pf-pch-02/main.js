cfdafd8e1d6008a4779b1694fd030c7905d58538
Rolf Mueller <rolf.mueller.au@gmail.com>
2012-03-23 15:16:59 +1100

    V2.1

--- jive/collab-pf-pch-02/main.js
+++ jive/collab-pf-pch-02/main.js
@@ -187,16 +187,14 @@ function checkAppData() {
 }
 
 function responseCheckAppData(obj) {
-    mini.createDismissibleMessage("responseCheckAppData() started...");
+    //mini.createDismissibleMessage("responseCheckAppData() started...");
     var lf_domdata = obj.data;
 //--- get message table
     var lf_elMessages = lf_domdata.getElementsByTagName('EX_T_MESSAGE')[0];
     for( var x = 0; x < lf_elMessages.childNodes.length; x++ ) {
         var lf_elItem = lf_elMessages.childNodes[x];
-//      var lf_message = lf_elItem.getElementsByName('MESSAGE')[0].childNodes[0].value;
-        var lf_elMessage = lf_elItem.getElementsByTagName('MESSAGE')[0];
-        var lf_Message = lf_elMessage.childNodes[0].nodeValue;
-        mini.createDismissibleMessage(lf_Message);
+        var lf_message = lf_elItem.getElementsByTagName('MESSAGE')[0].childNodes[0].nodeValue;
+        mini.createDismissibleMessage(lf_message);
     }
 
 }
