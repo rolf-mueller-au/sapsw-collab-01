3aca1a9185aecc0b13211344a6e288f38b6832bc
Rolf Mueller <rolf.mueller.au@gmail.com>
2012-04-25 21:28:00 +1000

    V3.5

--- sapsw/collab-pf-pch-03/main.js
+++ sapsw/collab-pf-pch-03/main.js
@@ -156,7 +156,8 @@ function responseLoadPernrDetails(obj) {
     //mini.createDismissibleMessage("responseLoadPernrDetails() started...");
     var lf_domdata = obj.data;
 //--- retrieve display name
-    pa_name.value = lf_domdata.getElementsByTagName('EX_F_DISPLAY_NAME')[0].childNodes[0].nodeValue;
+    if (typeof(lf_domdata.getElementsByTagName('EX_F_DISPLAY_NAME')[0].childNodes[0].nodeValue)!=='undefined')
+      { pa_name.value = lf_domdata.getElementsByTagName('EX_F_DISPLAY_NAME')[0].childNodes[0].nodeValue; }
 //--- retrieve BUKRS
     pa_bukrs_old.value = lf_domdata.getElementsByTagName('EX_F_BUKRS')[0].childNodes[0].nodeValue;
 //--- retrieve BUKRS_TXT
