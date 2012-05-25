9c47c8178c91874766adf2269d537fd9c2358069
Rolf Mueller <rolf.mueller.au@gmail.com>
2012-05-25 11:48:15 +1000

    V5.1

--- sapsw/collab-pf-pch-05/main.js
+++ sapsw/collab-pf-pch-05/main.js
@@ -596,7 +596,7 @@ function responsePchRead(obj) {
 //--- If PA_PERNR is empty, then we still have to register
     var lf_pa_pernr = document.getElementById('pa_pernr');
     if (lf_pa_pernr==null || typeof(lf_pa_pernr)=='undefined' ) {
-    } else { if (lf_pa_pernr.value=='') showRegistrationButtons(); }
+    } else { if (lf_pa_pernr.value=='' || lf_pa_pernr.value=='00000000') showRegistrationButtons(); }
 
 
 //--- Operation failed, let's out a generic warning
